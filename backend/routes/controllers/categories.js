61bfb6064931babc3a151316a6c73f7a7742d620
anyuka007 <anna.popova.de@gmail.com>
2024-10-13 14:47:26 +0200

    Refactor getCategories route to support pagination and sorting

--- backend/routes/controllers/categories.js
+++ backend/routes/controllers/categories.js
@@ -2,7 +2,17 @@ import Category from "../../models/Category.js";
 
 export const getCategories = async (req, res) => {
     try {
-        const categories = await Category.find();
+        const limit = Number(req.query.limit) || 0;
+        const currentPage = Number(req.query.currentPage) || 1;
+        const skip = (currentPage - 1) * limit;
+        const sortDirection = req.query.sortDirection === "asc" ? 1 : -1;
+        const sortField = req.query.sortBy;
+
+        const categories = await Category.find()
+            .limit(limit)
+            .skip(skip)
+            .sort({ [sortField]: sortDirection });
+
         console.log(
             `There are ${categories.length.toString().magenta} categories`
         );
